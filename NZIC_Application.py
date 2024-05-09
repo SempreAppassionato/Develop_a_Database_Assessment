3af7ff0c7c992beda16add8e9ad1f5a591e2ac6e
iamnotjomatusda <22343@burnside.school.nz>
2024-05-09 15:16:38 +1200

    Setup the main .py file with structure

---
+++ NZIC_Application.py
@@ -0,0 +1,16 @@
+# Name Last name (This is a synced to a public repository)
+# This application helps users better understand the results from Round 1 of NCIC 2024
+
+# Constants and variables 
+
+
+
+
+# Functions
+
+
+
+
+
+
+# Main code

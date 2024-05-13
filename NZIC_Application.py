224f8d4d5f04cb0d603ea350aaaba0aaca8c1c7a
iamnotjomatusda <22343@burnside.school.nz>
2024-05-13 13:30:23 +1200

    Set up the initial structure for the database application

--- NZIC_Application.py
+++ NZIC_Application.py
@@ -1,4 +1,4 @@
-# Name Last name (This is a synced to a public repository)
+# First Name Last name (This is a synced to a public repository)
 # This application helps users better understand the results from Round 1 of NCIC 2024
 
 # Constants and variables 

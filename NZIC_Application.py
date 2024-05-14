45f2b52c8f2877816b40e9602d25f02e1dc06ad2
iamnotjomatusda <22343@burnside.school.nz>
2024-05-14 13:25:40 +1200

    - Improved authentication methods - Added function to input custom SQL queries

--- NZIC_Application.py
+++ NZIC_Application.py
@@ -12,6 +12,10 @@ Development next steps:
 
 import sqlite3
 
+
+
+
+
 class color:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
@@ -26,16 +30,23 @@ class color:
 
 # Constants and variables 
 DATABASE = "NZIC.db"
-
+global username
+global password
 
 # Functions
-def authenticate_user(username, password):
+def authenticate_user():
+    username = input("username: ")
+    password = input("password: ")
     if username == "admin" and password == "password":
         return True
-    
-def root_user_authentication(username, password):
-    if username == "root" and password == "§¢£§¶§©∆":
+
+def root_user_authentication():
+    if username == "root" and password == "raspberrypi":
         return True
+    else:
+        username = input("username: ")
+        password = input("password: ")
+        root_user_authentication()
 
 def print_all_contestants():
     db = sqlite3.connect(DATABASE)
@@ -47,15 +58,42 @@ def print_all_contestants():
         print(color.BOLD + "Name: " + color.END + item[1] + color.BOLD + " Username: " + color.END + item[0])
     db.close()
 
+def custom_query(userquery):
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    cursor.execute(userquery)
+    results = cursor.fetchall()
+    for item in results:
+        print(item)
+    db.close()
+
 # Main code
+
+# Initial authentication 
 while True:
-    username = input("username: ")
-    password = input("password: ")
-    if authenticate_user(username, password):
+    if authenticate_user():
         print("Welcome!")
         break
     else:
         print("Invalid username or password. Please try again.")
         continue
 
-print_all_contestants()
+# Deciding what to do
+while True:
+    print("What would you like to do?")
+    print("1 - View all contestants")
+    print("2 - Enter a custom query")
+    choice = input(": ")
+    if choice == "1":
+        print_all_contestants()
+    if choice == "2":
+        username = input("username: ")
+        password = input("password: ")
+        if root_user_authentication(username, password):
+            while True:
+                userquery = input("Enter a custom SQL query: ")
+                custom_query(userquery)
+                break
+        else:
+            print("Invalid username or password. Please try again.")
+            continue

9f0e2a04ea85992502dd8c271e9b14ad4feab207
SempreAppassionato <106906047+SempreAppassionato@users.noreply.github.com>
2024-06-03 17:16:31 +1200

    - improved the security of input prompts in the authentication functions - heavily improved the root_user_authentication function - in the process of creating the rank_inside_school function

--- NZIC_Application.py
+++ NZIC_Application.py
@@ -53,18 +53,27 @@ def authenticate_user(username, password): # added to main code
 def root_user_authentication(): # added to main code
     global username
     global password
+    print(colour.RED + "Checking user priveleges. (Root User required)" + colour.END)
     if username == "admin" and password == "password":
         print(colour.DARKCYAN + "Root user authentication required." + colour.END)
-        username = input("username: ")
-        password = input("password: ")
-        root_user_authentication()
+    if username == "root" and password == "raspberrypi":
+        return True
+    try:
+        username = str(input("username: "))
+        password = str(input("password: "))
+    except:
+        pass
     if username == "root" and password == "raspberrypi":
         return True
     else:
         print(colour.RED + "Invalid username or password. Please try again.\n" + colour.END)
-        username = input("username: ")
-        password = input("password: ")
-        root_user_authentication()
+        while True:
+            print("Try again? (y/n)")
+            choice = input(": ")
+            if choice == "y" or choice == "Y":
+                root_user_authentication()
+            else:
+                break
 
 # Query functions
 def print_all_contestants(): # Main code 1
@@ -74,32 +83,7 @@ def print_all_contestants(): # Main code 1
     for item in results:
         print(f"{item[1]:<40}{item[0]:<40}{item[2]:<40}\n")
 
-def custom_query(): # main code CLEANED UP
-    global dangerous
-    dangerous = False
-    print(colour.BOLD + "Please enter your SQL query below" + colour.END)
-    try:
-        userquery = str(input(": "))
-    except:
-        print(colour.RED + "Invalid query, please try again." + colour.END)
-    dangerousKeywords = ["drop", "delete"]
-    if any(keyword in userquery.lower() for keyword in dangerousKeywords):
-        print(colour.RED + "Invalid query, please do not try again." + colour.END)
-        dangerous = True
-    concerningKeywords = ["update", "insert", "create", "replace"]
-    if any(keyword in userquery.lower() for keyword in concerningKeywords):
-        choice = are_you_sure()
-        if choice == 2:
-            dangerous = True
-        elif choice == 1:
-            dangerous = False
-    if dangerous != True:
-        results = run_sql(userquery)
-        if results != 1:
-            for item in results:
-                print(item)
-
-def individual_score(username=None): # main code 3
+def individual_score(username=None): # JOINED main code 2
     try:
         if username != None:
             userIdentification = username
@@ -124,7 +108,7 @@ def individual_score(username=None): # main code 3
     except:
         print(colour.RED + "Invalid query, please try again.\n" + colour.END)
 
-def search_username(): # main code 3
+def search_username(): # JOINED main code 2
     global resultsList
     try:
         search = str(input(colour.BOLD + "Please enter a username or real name to search for: " + colour.END))
@@ -143,14 +127,39 @@ def search_username(): # main code 3
     except:
         print(colour.RED + "Invalid query, please try again." + colour.END)
 
-def rank_by_total_score(): # main code 5
-    sql = "select rank, real_name, username from User order by rank asc"
-    results = run_sql(sql)
-    print(colour.BOLD + colour.UNDERLINE + "Rank" + " Name" + "                                    " + "Username                            " + colour.END)
-    for item in results:
-        print(f"{item[0]:<5}{item[1]:<40}{item[2]:<40}")
+def custom_query(): # main code 3
+    global dangerous
+    dangerous = False
+    print(colour.BOLD + "Please enter your SQL query below" + colour.END)
+    try:
+        userquery = str(input(": "))
+    except:
+        print(colour.RED + "Invalid query, please try again." + colour.END)
+    dangerousKeywords = ["drop", "delete"]
+    if any(keyword in userquery.lower() for keyword in dangerousKeywords):
+        print(colour.RED + "Invalid query, please do not try again." + colour.END)
+        dangerous = True
+    concerningKeywords = ["update", "insert", "create", "replace"]
+    if any(keyword in userquery.lower() for keyword in concerningKeywords):
+        choice = are_you_sure()
+        if choice == 2:
+            dangerous = True
+        elif choice == 1:
+            dangerous = False
+    if dangerous != True:
+        results = run_sql(userquery)
+        if results != 1:
+            for item in results:
+                print(item)
 
-def rank_by_question_score(): # main code y
+def rank_by_total_score(): # main code 4
+    sql = "select rank, real_name, username, sum(question_score) from Question_score, User where user.id = Question_score.user_id group by user.id order by rank asc;"
+    results = run_sql(sql)
+    print(colour.BOLD + colour.UNDERLINE + "Rank" + " Name" + "                                    " + "Username                            " + "Score" + colour.END)
+    for item in results:
+        print(f"{item[0]:<5}{item[1]:<40}{item[2]:<36}{item[3]:<5}")
+
+def rank_by_question_score(): # main code 5
     try:
         questionID = int(input("Please enter the a question number (1, 2, 3, 4, or 5): "))
     except:
@@ -162,14 +171,77 @@ def rank_by_question_score(): # main code y
         cursor.execute(sql, (questionID,))
         results = cursor.fetchall()
         print("The question is " + results[0][2] + " and the maximum points is " + str(results[0][4]) + ".")
-        print(colour.BOLD + colour.UNDERLINE + "Rank" + " Name" + "                                    " + "Username                                " + "Points       "+ colour.END)
+        print(colour.BOLD + colour.UNDERLINE + "Question Rank" + " Name" + "                                    " + "Username                                " + "Points       "+ colour.END)
         i = 1
         for item in results:
-            print(f"{i:<5}{item[0]:<40}{item[1]:<40}{item[3]:<40}")
+            print(f"{i:<10}    {item[0]:<40}{item[1]:<40}{item[3]:<40}")
             i += 1
     except:
         print(colour.RED + "Invalid question number, please try again.\n" + colour.END)
     db.close()
+
+def rank_inside_school():
+    # select real_name, username, sum(question_score) from Question_score, User where (user.id = Question_score.user_id and user.school = ?) group by user.id order by rank asc;
+    print(colour.BOLD + "\nThe following schools had students who participated in this round: " + colour.END)
+    print("""
+    Macleans College
+    Rangitoto College
+    Burnside High School
+    Auckland Grammar School
+    Pinehurst School
+    ACG Parnell College
+    Wellington College
+    Kristin School
+    Mount Albert Grammar School
+    Murrays Bay Intermediate School
+    Avondale College
+    Ormiston Senior College
+    King's College
+    Lynfield College
+    Newlands College
+    One Tree Hill College
+    Long Bay College
+    Lincoln High School
+    Rototuna High School
+    Bucklands Beach Intermediate
+    Hamilton Boys' High School
+    Papanui High School
+    Wellington Girls' College
+    St Andrew's College
+    New Plymouth Boys' High School
+    Kingsway School
+    Scots College
+    James Hargest College
+    Verdon College
+    Saint Patrick's College Silverstream
+    Homeschooled
+    Belmont Intermediate
+    Hillcrest High School
+    Mount Roskill Grammar
+    Westlake Boys High School
+    Saint Kentigern Boys' School
+    BestCoder
+    St Cuthbert's College
+    Patumahoe Primary School
+    Bayfield High School
+    Howick College
+    Takapuna Normal Intermediate School
+    Wellington Girls' College
+    Otumoetai College
+    Diocesan School for Girls
+    Mount Aspiring College
+    Queen's High School
+    Ficino School
+    Hutt Valley High School
+    TAM Code Club
+    Wentworth Computer Science College
+    Cashmere High School
+    Saint Kentigern College
+    ACG Sunderland
+    Amesbury School
+    Newlands Intermediate""")
+
+    school = input("Please enter the name of the school you would like to see the ranking for: ")
 
 # other functions
 def are_you_sure(): # used to ask the user if they are sure they want to run a dangerous query
@@ -204,10 +276,15 @@ def run_sql(query):
 
 # other functions
 
+
+#rank_inside_school()
 # MAIN CODE __________________________________________________________________
 while True: # Initial authentication 
-    username = input("username: ")
-    password = input("password: ")
+    try:
+        username = str(input("username: "))
+        password = str(input("password: "))
+    except:
+        print(colour.RED + "Error\n" + colour.END)
     login = authenticate_user(username, password)
     if login == 1:
         print(colour.GREEN + "Login succeded, you have admin privileges.\n" + colour.END)
@@ -223,26 +300,26 @@ while True: # Main menu
     time.sleep(0.7)
     print("\n\n----------------------------------------------")
     print("1 - View all contestants")
-    print("3 - Search for a username or real name to see user details")
-    print("4 - Enter a custom SQL query (root access required)")
-    print("5 - View the overall ranking by total score")
-    print("6 - View the ranking by score on a specific question")
+    print("2 - Search for a username or real name to see user details")
+    print("3 - Enter a custom SQL query (root access required)")
+    print("4 - View the overall ranking by total score")
+    print("5 - View the ranking by score on a specific question")
     print(colour.PURPLE + "\nPress press " + colour.BOLD + "q" + colour.END + colour.PURPLE + " to exit" + colour.END)
     print("----------------------------------------------\n")
     choice = input(": ")
     if choice == "1":
         print_all_contestants()
-    elif choice == "3":
+    elif choice == "2":
         search_username()
         for user in resultsList:
             individual_score(user)
         resultsList = [] # clearing the results list 
-    elif choice == "4":
+    elif choice == "3":
         if root_user_authentication():
             custom_query()
-    elif choice == "5":
+    elif choice == "4":
         rank_by_total_score()
-    elif choice == "6":
+    elif choice == "5":
         rank_by_question_score()
     elif choice == "q" or choice == "Q" or choice == "q " or choice == "Q ":
         print(colour.GREEN + "\n\nHow was your experience of this application? Your feedback will be sincerely appreciated. \nContact: 22343@burnside.school.nz\n\n\n\n")
